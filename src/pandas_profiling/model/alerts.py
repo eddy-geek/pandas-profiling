fix: fix issues with constant computation

src/pandas_profiling/model/alerts.py
@@ -329,7 +329,11 @@ def alert_value(value: float) -> bool:
 
 
 def skewness_alert(v: float, threshold: int) -> bool:
-    return not np.isnan(v) and (v < (-1 * threshold) or v > threshold)
+    # if v is None (skewness not computed), do not check
+    if v:
+        return not np.isnan(v) and (v < (-1 * threshold) or v > threshold)
+    else:
+        return False
 
 
 def type_date_alert(series: pd.Series) -> bool: